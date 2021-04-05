da16db5930f8c1f2c9fcb30227234624958cc783
Javier Carrara <jacarrara@gmail.com>
2021-04-05 10:05:38 -0300

    added windows script

---
+++ install_grc_resources.ps1
@@ -0,0 +1,11 @@
+﻿if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
+Set-ExecutionPolicy Bypass -Scope Process -Force
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072 
+iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+choco install python3 -y
+
+pip install awscli git-remote-codecommit
+
+write-host ""
+Read-Host 'Press Enter to continue…'
